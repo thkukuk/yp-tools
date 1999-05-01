aac6f11f0e980359896166e0d85ae2a6410f6aab
Thorsten Kukuk <kukuk@thkukuk.de>
1999-05-01 17:32:04 +0000

    *** empty log message ***

--- acconfig.h
+++ acconfig.h
@@ -1,5 +1,5 @@
 /* Special definitions, processed by autoheader.
-   Copyright (C) 1997 Thorsten Kukuk <kukuk@vt.uni-paderborn.de> */
+   Copyright (C) 1997, 1999 Thorsten Kukuk <kukuk@suse.de> */
 
 /* Define to the name of the distribution.  */
 #undef PACKAGE
