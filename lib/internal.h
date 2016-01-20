ea66fde92c228bf7b13612b07e4cf1b0667e8481
Thorsten Kukuk <kukuk@thkukuk.de>
2016-01-20 14:00:44 +0100

    The do_ypcall_tr "resp" parameter is type caddr_t. This necessitates a cast to ypresp_val * to reference resp->status for use as an argument to ypprot_err which provides the return value for do_ypcall_tr.
    Since caddr_t and ypresp_val * can have different alignments on some archs such
    as arm, GCC -Wcast-align will issue a warning which yp-tools is configured to
    treat as an error.
    
    This commit changes the type of the resp parameter of do_ypcall_tr from caddr_t
    to ypresp_val * in order to avoid the posibility of an unaligned cast.
    
    Signed-off-by: Jonathan Ben Avraham <yba@tkos.co.il>
    Signed-off-by: Thorsten Kukuk <kukuk@thkukuk.de>

--- lib/internal.h
+++ lib/internal.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Thorsten Kukuk
+/* Copyright (C) 2014, 2016 Thorsten Kukuk
    Author: Thorsten Kukuk <kukuk@suse.de>
 
    This library is free software: you can redistribute it and/or
@@ -19,6 +19,6 @@
 extern int do_ypcall (const char *domain, u_long prog, xdrproc_t xargs,
 		      caddr_t req, xdrproc_t xres, caddr_t resp);
 extern int do_ypcall_tr (const char *domain, u_long prog, xdrproc_t xargs,
-			 caddr_t req, xdrproc_t xres, caddr_t resp);
+			 caddr_t req, xdrproc_t xres, struct ypresp_val *resp);
 extern int yp_maplist (const char *, struct ypmaplist **);
 #endif
